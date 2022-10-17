d9406bef99a5a2cc05a1c5c97b7234eeb4fb674c
bruninha2511 <brunaluch2511@gmail.com>
2022-10-17 19:50:39 -0300

    Aula_1

---
+++ SRC/Aula1.py
@@ -0,0 +1,32 @@
+# funções importantes
+
+
+
+print('oi') # mostra uma palavra no terminal
+#input('digite uma palavra') # pede para digitar algo no terminal
+
+# variaveis
+# simples
+a=1
+b=1.5
+c='bruna luchtenberg'
+d=True
+print(a,b,c,d)
+print(type(a),type(b),type(c),type(d))
+
+# operações matematicas
+a=2
+b=3
+print(f'soma: {a+b}')
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) # pega o inteiro da divisão
+print(a**b) # faz exponencial
+print(a%b) # pega o resto da divisão
+
+print(c.upper()) # deixa tudo maiusculo
+print(c.lower()) # deixa tudo minusculo
+print(c.capitalize()) # deixa só a primeira letra do texto maiuscula
+print(c.title()) # deixa toda inicial maiuscula
+print(c.strip()) # retira os espaços antes e depois da palavra, mas n entre ela
